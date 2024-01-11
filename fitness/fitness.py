dd6e965f0269c987d68e059f35060b826db7d7c9
bouncingdiver <euwilson066@gmail.com>
2024-01-11 23:55:41 +0000

    Made a basic ask_fruit function and shifted it to the top

--- fitness/fitness.py
+++ fitness/fitness.py
@@ -1,1 +1,8 @@
+def ask_fruit():
+    number_of_fruit = input("How many pieces of fruit have you eaten today this week? \n")
+
+    print("You  have eaten " + number_of_fruit + " pieces of fruit today. Well done.")
+
 print("Remember, your weekly goal is to achieve eating 3 pieces of fruit and 3 excercise sessions that are an hour long.")
+
+ask_fruit()
