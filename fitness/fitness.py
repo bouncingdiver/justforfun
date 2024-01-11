de97d62c9e1748a560d67252437379578a57a3c7
bouncingdiver <euwilson066@gmail.com>
2024-01-11 23:59:13 +0000

    Added 2 empty lines after the initial message, more visually pleasing

--- fitness/fitness.py
+++ fitness/fitness.py
@@ -3,6 +3,6 @@ def ask_fruit():
 
     print("You  have eaten " + number_of_fruit + " pieces of fruit today. Well done.")
 
-print("Remember, your weekly goal is to achieve eating 3 pieces of fruit and 3 excercise sessions that are an hour long.")
+print("Remember, your weekly goal is to achieve eating 3 pieces of fruit and 3 excercise sessions that are an hour long.\n\n")
 
 ask_fruit()
