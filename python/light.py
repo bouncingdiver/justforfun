f215bd95eac57af5d4edde18f0218fb1f3d5a693
bouncingdiver <euwilson066@gmail.com>
2024-01-16 21:15:51 +0000

    Added the pin variables

--- python/light.py
+++ python/light.py
@@ -3,3 +3,7 @@ import machine, time
 redLedPin = 15
 yellowLedPin = 14
 greenLedPin = 13
+
+redLed = machine.Pin(redLedPin, machine.Pin.OUT)
+yellowLed = machine.Pin(yellowLedPin, machine.Pin.OUT)
+greenLed = machine.Pin(greenLedPin, machine.Pin.OUT)
